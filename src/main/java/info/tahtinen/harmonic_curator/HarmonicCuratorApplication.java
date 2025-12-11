0dda4c1d326b51d51f835dc023f75f016c3dc1f3
Mika Tähtinen <mika@tahtinen.info>
2025-12-10 20:50:26 -0500

    Initialize Spring Boot project with Gradle setup and essential configuration files

---
+++ src/main/java/info/tahtinen/harmonic_curator/HarmonicCuratorApplication.java
@@ -0,0 +1,13 @@
+package info.tahtinen.harmonic_curator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonicCuratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonicCuratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/tahtinen/harmonic_curator/HarmonicCuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package info.tahtinen.harmonic_curator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonicCuratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
